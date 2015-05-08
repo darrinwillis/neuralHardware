3369f2834ec2336a2623c78f5084a592328f0e38
darrinwillis <willisdarrin@gmail.com>
2015-05-07 21:22:53 -0400

    Fixed multiplier; now works

--- src/neuralFPGA/ChipInterface.sv
+++ src/neuralFPGA/ChipInterface.sv
@@ -1,7 +1,7 @@
 module ChipInterface
     (input  logic   CLOCK_50,
-     input  logic[9:0] SW,
-     input  logic[2:0] KEY,
+     input  logic[17:0] SW,
+     input  logic[3:0] KEY,
      output logic[6:0] HEX7, HEX6 ,HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
 
     SimpleTest st(
@@ -14,19 +14,27 @@ endmodule: ChipInterface
 
 module SimpleTest
     (input  bit   clk,
-     input  bit[9:0] sw,
-     input  bit[2:0] key,
+     input  bit[17:0] sw,
+     input  bit[3:0] key,
      output bit [7:0][6:0] hexDisplays);
 
-	bit [7:0][3:0] values;
+	bit [31:0] constantA, constantB, mult, resultA, resultB;
+	assign constantA = 32'hfffe_8000;
+	assign constantB = 32'h0001_0000;
 	assign values = {4'd1, 4'd2, 4'd3, 4'd4, 4'd5, 4'd6, 4'd7, 4'd8};
+	assign mult = {8'd0, sw[15:0], 8'd0};
+
+	fixed_point_multiplier fpmA(.dataa(constantA), .datab(mult), .result(resultA));
+
+	fixed_point_multiplier fpmB(.dataa(constantB), .datab(mult), .result(resultB));
 
     //Generate the seven segment display
     genvar k;
     generate
         for (k = 0; k < 8; k=k+1) begin : SEV_SEG
             SevenSegmentDigit ssd(
-                .bch(values[k]),
+                //.bch((k > 3) ? resultB[31-4*(k-4) : 31-4*(k-3)] : resultA[31-4*k : 31-4*(k+1)]),
+                .bch(resultA[4*(k+1)-1 : 4*k]),
                 .segment(hexDisplays[k]),
                 .blank(1'b0));
             end
--- src/neuralFPGA/fixed_point_multiplier.sv
+++ src/neuralFPGA/fixed_point_multiplier.sv
@@ -3,39 +3,16 @@ module fixed_point_multiplier (
 	input bit[31:0] dataa, datab,
 	output bit[31:0] result);
 
-	bit[15:0] intA, fracA, intB, fracB;
-	bit[31:0] iAiB, iAfB, fAiB, fAfB;
+	bit[63:0] rawResult;
 
-	assign intA = dataa[31:16]; 
-	assign fracA = {dataa[31], dataa[15:1]};
-	assign intB = datab[31:16];
-	assign fracB = {datab[31], datab[15:1]};
+	multiplier_32 int1int2(
+		.dataa(dataa),
+		.datab(datab),
+		.result(rawResult));
 
-	multiplier_16 int1int2(
-		.dataa(intA),
-		.datab(intB),
-		.result(iAiB));
-
-	multiplier_16 int1frac2(
-		.dataa(intA),
-		.datab(fracB),
-		.result(iAfB));
-
-	multiplier_16 frac1int2(
-		.dataa(fracA),
-		.datab(intB),
-		.result(fAiB));
-
-	multiplier_16 frac1frac2(
-		.dataa(fracA),
-		.datab(fracB),
-		.result(fAfB));
 
 	// Shift over all bits appropriately and add
 
-	assign result = {iAiB, 16'd0} + 
-					iAfB +
-					fAiB +
-					{fAfB[31] ? 16'hffff : 16'h0000, fAfB[30:15]};
+	assign result = rawResult[47:16];
 
 endmodule: fixed_point_multiplier
