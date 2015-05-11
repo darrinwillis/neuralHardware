6255e41d27a5d1ca370eacbced79454912b5c658
darrinwillis <willisdarrin@gmail.com>
2015-05-11 03:20:58 -0400

    FSM correctly loads datapoints within an iteration of a run.

--- src/neuralFPGA/neuralProcessor.sv
+++ src/neuralFPGA/neuralProcessor.sv
@@ -3,7 +3,7 @@ module neuralProcessor (
 	input bit train, test,
 	input bit[7:0] test_sel,
 	input bit[31:0] mem_data,
-	output bit[31:0] address,
+	output bit[10:0] address,
 	output bit[31:0] test_output);
 
 	// Control Signals for FSM
@@ -11,68 +11,63 @@ module neuralProcessor (
 	bit  	load_num_train,
 			load_num_test,
 			load_mem, 
-			load_sum,
-			load_hout,
 			load_output,
 			load_oerror,
 			load_herror,
 			load_new_weights,
 			inc_address,
 			rst_address,
-			store_test_out;
+			store_test_out,
+			ready_to_test;
 	bit[2:0] input_sel;
 
 	// Meta-data
 	bit[7:0] numTest, numTrain;
-
-	// Weights
 	bit[31:0] learn_rate;
 	bit[31:0] iw; // initial weight
 	assign iw = 32'h0000_3800; // 1/8 + 1/16 + 1/32 ~ 0.22 ~ 0.2
-	bit[4:0][31:0] input_values;
-	bit[3:0][3:0][31:0] input_to_hidden_weights;
-	bit[3:0][31:0] hidden_to_output_weights;
+
+	// Parameters
 	bit[3:0][3:0][31:0] hidden_products;
 	bit[3:0][31:0] hidden_sums;
 	bit[3:0][31:0] hidden_outputs;
 	bit[3:0][31:0] output_products;
+
+	// Stored values (not intermediate calculations)
+	bit[4:0][31:0] input_values;
+	bit[3:0][3:0][31:0] stored_hidden_weights;
+	bit[3:0][31:0] stored_output_weights;
+	bit[3:0][31:0] stored_hidden_outputs;
+	bit[31:0] stored_final_output;
+
+	// Update parameters info
 	bit[31:0] final_output;
 	bit[31:0] output_error, output_diff, output_inversion, output_err_temp;
 	bit[3:0][31:0] hidden_error, hidden_inversion, hidden_err_temp1, hidden_err_temp2;
-	bit[3:0][3:0][31:0] hidden_correction, hidden_correc_temp;
+	bit[3:0][3:0][31:0] hidden_correction;
+	bit[3:0][31:0] hidden_correc_temp;
 	bit[3:0][31:0] out_correction;
 	bit[31:0] out_error_temp;
+
+	// Stored error values
+	bit[31:0] stored_output_error;
+	bit[3:0][31:0] stored_hidden_error;
+
+	assign last_train = (address - 2) == numTrain;
 
     always_ff @(posedge clk, posedge rst) begin
         if (rst) begin
             numTest <= 0;
             numTrain <= 0;
             learn_rate <= 32'h0000_4000; // 0.25
-            input_to_hidden_weights[3][3] <= iw;
-            input_to_hidden_weights[3][2] <= iw;
-            input_to_hidden_weights[3][1] <= iw;
-            input_to_hidden_weights[3][0] <= iw;
-
-            input_to_hidden_weights[2][3] <= iw;
-            input_to_hidden_weights[2][2] <= iw;
-            input_to_hidden_weights[2][1] <= iw;
-            input_to_hidden_weights[2][0] <= iw;
-
-            input_to_hidden_weights[1][3] <= iw;
-            input_to_hidden_weights[1][2] <= iw;
-            input_to_hidden_weights[1][1] <= iw;
-            input_to_hidden_weights[1][0] <= iw;
-
-            input_to_hidden_weights[0][3] <= iw;
-            input_to_hidden_weights[0][2] <= iw;
-            input_to_hidden_weights[0][1] <= iw;
-            input_to_hidden_weights[0][0] <= iw;
-
-            hidden_to_output_weights[3] <= iw;
-            hidden_to_output_weights[2] <= iw;
-            hidden_to_output_weights[1] <= iw;
-            hidden_to_output_weights[0] <= iw;
-
+            address <= 11'd0;
+            test_output <= 32'd0;
+        	for (int i=0; i < 4; i++) begin
+				for (int l=0; l < 4; l++) begin
+            		stored_hidden_weights[i][l] <= iw;
+            		end
+            	stored_output_weights[i] <= iw;
+           		end
             end
         else begin
         	if (load_mem) begin
@@ -87,17 +82,53 @@ module neuralProcessor (
             if (load_new_weights) begin
             	for (int i=0; i < 4; i++) begin
             		for (int l=0; l < 4; l++) begin
-            			input_to_hidden_weights[i][j] =
-            				input_to_hidden_weights[i][j] + hidden_correction[i][j];
+            			stored_hidden_weights[i][l] <=
+            				stored_hidden_weights[i][l] + hidden_correction[i][l];
             			end
-            		hidden_to_output_weights[i] = 
-            			hidden_to_output_weights[i] + out_correction[i];
+            		stored_output_weights[i] <= 
+            			stored_output_weights[i] + out_correction[i];
             		end
+            	end
+            if (load_output) begin
+            	stored_hidden_outputs <= hidden_outputs;
+            	stored_final_output <= final_output;
+            	end
+            if (load_oerror) begin
+            	stored_output_error <= output_error;
+            	end
+            if (load_herror) begin
+            	stored_hidden_error <= hidden_error;
+            	end
+           	if (inc_address) begin
+           		address <= address +1;
+           		end
+            if (rst_address) begin
+            	address <= 11'd2;
+            	end
+            if (store_test_out) begin
+            	test_output <= stored_final_output;
+            	end
+            if (ready_to_test) begin
+            	test_output <= 32'hD00D_B00B;
             	end
             end
         end
 
 	neuralFSM NFSM(.*);
+
+	always_comb begin
+		for (int k=0; k<4; k++) begin: ADDER_OUTER
+				hidden_sums[k] = hidden_products[0][k] + 
+								 hidden_products[1][k] +
+								 hidden_products[2][k] +
+								 hidden_products[3][k];
+			end
+		end
+
+	assign final_output = output_products[0] + 
+						  output_products[1] +
+						  output_products[2] +
+						  output_products[3];
 
 	// Create all of the hidden outputs and sums
 	genvar k, j;
@@ -105,18 +136,10 @@ module neuralProcessor (
 		for (k=0; k<4; k++) begin: HID_MULTS_OUTER
 			for (j=0; j < 4; j++) begin: HID_MULTS_INNER
 				fixed_point_multiplier HIDMULT(
-					.dataa(input_to_hidden_weights[k][j]),
+					.dataa(stored_hidden_weights[k][j]),
 					.datab(input_values[k]),
 					.result(hidden_products[k][j]));
 			end
-
-			// Sum contributions from all inputs
-			adder4_32 summer(
-				.data0x(hidden_products[0][k]),
-				.data1x(hidden_products[1][k]),
-				.data2x(hidden_products[2][k]),
-				.data3x(hidden_products[3][k]),
-				.result({2'b00, hidden_sums[k]}));
 
 			// Perform activation function on sum
 			sigmoid sigm(
@@ -125,46 +148,40 @@ module neuralProcessor (
 
 			fixed_point_multiplier OUTMULT(
 				.dataa(hidden_outputs[k]),
-				.datab(hidden_to_output_weights[k]),
+				.datab(stored_output_weights[k]),
 				.result(output_products[k]));
 		end
 	endgenerate
 
-	adder4_32 sumOut(
-		.data0x(output_products[0]),
-		.data1x(output_products[1]),
-		.data2x(output_products[2]),
-		.data3x(output_products[3]),
-		.result({2'b00, final_output}));
-
 	// Generate the error function
-	assign output_diff = input_values[4] - final_output;
-	assign output_inversion = 32'h0001_0000 - final_output;
+	assign output_diff = input_values[4] - stored_final_output;
+	assign output_inversion = 32'h0001_0000 - stored_final_output;
 	fixed_point_multiplier OERR_MULT(
 		.dataa(output_diff),
 		.datab(output_inversion),
 		.result(output_err_temp));
 	fixed_point_multiplier OERR_FINAL_MULT(
 		.dataa(output_err_temp),
-		.datab(final_output),
+		.datab(stored_final_output),
 		.result(output_error));
 	
 	// Generate the hidden error functions
-	assign hidden_inversion[3] = 1 - hidden_outputs[3];
-	assign hidden_inversion[2] = 1 - hidden_outputs[2];
-	assign hidden_inversion[1] = 1 - hidden_outputs[1];
-	assign hidden_inversion[0] = 1 - hidden_outputs[0];
+	always_comb begin
+		for (int i = 0; i < 4; i++) begin
+			hidden_inversion[i] = 1 - stored_hidden_outputs[i];
+		end
+	end
 
 	generate
 		for (k=0; k<4; k++) begin: HID_ERRORS
 			fixed_point_multiplier HERR_TEMP1(
-				.dataa(hidden_outputs[k]),
+				.dataa(stored_hidden_outputs[k]),
 				.datab(hidden_inversion[k]),
 				.result(hidden_err_temp1[k]));
 
 			fixed_point_multiplier HERR_TEMP2(
-				.dataa(output_error),
-				.datab(hidden_to_output_weights[k]),
+				.dataa(stored_output_error),
+				.datab(stored_output_weights[k]),
 				.result(hidden_err_temp2[k]));
 
 			fixed_point_multiplier HERR_FINAL(
@@ -181,28 +198,28 @@ module neuralProcessor (
 
 			fixed_point_multiplier HID_CORRECT_TEMP1(
 				.dataa(learn_rate),
-				.datab(hidden_error[j]),
-				.result(hidden_correc_temp));
+				.datab(stored_hidden_error[k]),
+				.result(hidden_correc_temp[k]));
 
 			for (j=0; j < 4; j++) begin: HID_NEW_INNER
 				fixed_point_multiplier HID_CORRECT_TEMP1(
-					.dataa(hidden_correc_temp),
+					.dataa(hidden_correc_temp[k]),
 					.datab(input_values[j]),
-					.result(hidden_correction));
+					.result(hidden_correction[k][j]));
 			end
 		end
 	endgenerate
 
 	fixed_point_multiplier out_correc(
 		.dataa(learn_rate),
-		.datab(output_error),
+		.datab(stored_output_error),
 		.result(out_error_temp));
 	// Create new values for output weights
 	generate
 		for (k=0; k<4; k++) begin: OUT_NEW
 			fixed_point_multiplier HID_CORRECT_TEMP1(
 				.dataa(out_error_temp),
-				.datab(hidden_outputs[k]),
+				.datab(stored_hidden_outputs[k]),
 				.result(out_correction[k]));
 		end
 	endgenerate
@@ -214,8 +231,6 @@ module neuralFSM (
 	output bit  load_num_train,
 				load_num_test,
 				load_mem, 
-				load_sum,
-				load_hout,
 				load_output,
 				load_oerror,
 				load_herror,
@@ -223,13 +238,15 @@ module neuralFSM (
 				inc_address,
 				rst_address,
 				store_test_out,
+				ready_to_test,
 	output bit[2:0] input_sel
 );
 
 	logic[7:0] numIterations;
 	bit dec_iterations;
 	bit training_mode, load_mode;
-	enum logic[3:0] {INIT1, INIT2, WAIT, LOADED_TRAIN, LOADED_OUT,
+	bit inc_input_sel;
+	enum logic[3:0] {INIT1, INIT2, WAIT, HOLD, LOADING, LOADED_DATA, LOADED_OUT,
 					 O_ERR_CALCED, H_ERR_CALCED, DATUM_DONE, 
 					 LOADED_TEST, LOADED_TEST_OUT} cs, ns;
 
@@ -237,7 +254,7 @@ module neuralFSM (
         if (rst) begin
             cs <= INIT1;
             numIterations <= 8'd200;
-            input_sel <= 2'd0;
+            input_sel <= 3'd0;
             training_mode <= 'b0;
             end
         else begin
@@ -248,6 +265,9 @@ module neuralFSM (
             if (load_mode) begin
             	training_mode <= train;
             	end
+            if (inc_input_sel) begin
+            	input_sel <= input_sel == 5 ? 0 : input_sel + 1;
+            	end
         	end
         end
 
@@ -255,14 +275,17 @@ module neuralFSM (
     	load_num_train = 'b0;
     	load_num_test = 'b0;
         load_mem = 'b0;
-        load_sum = 'b0;
-        load_hout = 'b0;
         load_output = 'b0;
         load_oerror = 'b0;
         load_herror = 'b0;
         load_new_weights = 'b0;
         dec_iterations = 'b0;
         store_test_out = 'b0;
+        ready_to_test = 'b0;
+        inc_address = 'b0;
+        rst_address = 'b0;
+        load_mode = 'b0;
+        inc_input_sel = 'b0;
         case (cs) 
             INIT1: begin
                 ns = INIT2;
@@ -271,16 +294,26 @@ module neuralFSM (
             INIT2: begin
                 ns = WAIT;
                 load_num_test = 'b1;
+                rst_address = 'b1;
                 end
             WAIT:  begin
-                ns = train ? LOADED_TRAIN : (test ? LOADED_TEST : WAIT);
-                load_mem = train;
+                ns = (train | test) ? HOLD : WAIT;
+                inc_address = train | test;
+                load_mode = 'b1;
                 end
-            LOADED_TRAIN: begin
-            	ns = LOADED_OUT;
-            	load_sum = 'b1;
-            	load_hout = 'b1;
-            	load_output = 'b01;
+            HOLD: begin
+            	ns = LOADING;
+            	inc_address = 'b1;
+            	end
+            LOADING: begin
+            	ns = input_sel == 5 ? LOADED_DATA : LOADING;
+            	inc_input_sel = 'b1;
+            	load_mem = 'b1;
+            	inc_address = input_sel < 3;
+            	end
+            LOADED_DATA: begin
+            	ns = training_mode ? LOADED_OUT : LOADED_TEST;
+            	load_output = 'b1;
             	end
            	LOADED_OUT: begin
            		ns = O_ERR_CALCED;
@@ -296,10 +329,13 @@ module neuralFSM (
 	        	inc_address = 'b1;
 	        	end
 	        DATUM_DONE: begin
-	        	ns = last_train && numIterations == 0 ? WAIT : LOADED_TRAIN;
+	        	ns = last_train && numIterations == 0 ? WAIT : HOLD;
 	        	rst_address = last_train && numIterations != 0;
 	        	load_mem = ~(last_train && numIterations == 0);
+	        	inc_input_sel = ~(last_train && numIterations == 0);
+	        	inc_address = ~(last_train && numIterations == 0);
 	        	dec_iterations = 'b1;
+	        	ready_to_test = last_train && numIterations == 0;
 	        	end
 	        // Now just states dealing with testing, not training
 	        LOADED_TEST: begin
